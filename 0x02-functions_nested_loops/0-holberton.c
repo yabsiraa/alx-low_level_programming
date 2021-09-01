9328349f45e255746e67d1a4dfe8e3f40cf04198
yabsiraa <88306942+yabsiraa@users.noreply.github.com>
2021-09-01 12:11:07 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,24 @@
-include <stdio.h>
- 
-int main ()
+#include "main.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Program that prints Holberton, followed by a new line
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
 {
-   
- 
-   printf(Holberton /n );
- 
-   return 0;
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+		_putchar(10);
+
+	return (0);
 }
